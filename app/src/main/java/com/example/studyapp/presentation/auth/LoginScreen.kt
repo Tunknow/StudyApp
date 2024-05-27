01fe7f7a64ea94c68fbad2e0be1590ef29e4d621
QuangTung <tung75csp@gmail.com>
2024-05-27 09:40:22 +0700

    Change UI style in Login

--- app/src/main/java/com/example/studyapp/presentation/auth/LoginScreen.kt
+++ app/src/main/java/com/example/studyapp/presentation/auth/LoginScreen.kt
@@ -6,9 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,10 +21,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.example.studyapp.R
@@ -117,7 +127,14 @@ fun LoginScreen(
                     loginViewModel.showInvalidLoginDialog.value = false
                 },
                 title = {
-                    Text(text = "Đăng nhập không hợp lệ")
+                    Text(
+                        text = "Đăng nhập không hợp lệ",
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold,
+                            fontStyle = FontStyle.Normal
+                        ),
+                    )
                 },
                 text = {
                     Text(text = "Vui lòng kiểm tra lại email và mật khẩu.")
@@ -126,11 +143,30 @@ fun LoginScreen(
                     Button(
                         onClick = {
                             loginViewModel.showInvalidLoginDialog.value = false
-                        }
+                        },
+                        colors = ButtonDefaults.buttonColors(Color.Transparent)
                     ) {
-                        Text(text = "OK")
+                        Box(
+                            modifier = Modifier
+                                .width(72.dp)
+                                .heightIn(36.dp)
+                                .background(
+                                    brush = Brush.horizontalGradient(
+                                        listOf(
+                                            colorResource(id = R.color.colorPrimary),
+                                            colorResource(id = R.color.colorSecondary)
+                                        )
+                                    ),
+                                    shape = RoundedCornerShape(50.dp)
+                                ),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "OK")
+                        }
+
                     }
-                }
+                },
+                containerColor = Color.White
             )
         }
     }
